3d472b9f4674d8d85d00ebf89f87b11a7654b011
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-03 00:42:16 +0800

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import LogIn from "./pages/LogIn";
 import Register from "./pages/Register";
+
+import Main from "./layout/Main";
 import Dashboard from "./pages/Dashboard";
+import Audit from "./pages/Audit";
+import Library from "./pages/Library";
+import Help from "./pages/Help";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Login route and register */}
         <Route path="/login" element={<LogIn />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Dashboard />} />
+
+        {/* Main app routes */}
+        <Route path="/" element={<Main />}>
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="audit" element={<Audit />} />
+          <Route path="library" element={<Library />} />
+          <Route path="help" element={<Help />} />
+        </Route>
+
+        {/* Then catch all route */}
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
   );
