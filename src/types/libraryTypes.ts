66840c43dd729e230e9be84cea386dd6dd11d202
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-24 19:29:44 +0800

    Created types on a separate file

---
+++ src/types/libraryTypes.ts
@@ -0,0 +1,20 @@
+export type TabKey =
+  | "agencies"
+  | "auditors"
+  | "auditAreas"
+  | "auditCriteria"
+  | "typesOfAudit"
+  | "internalControls"
+  | "documentTypes"
+  | "userAccounts";
+
+export interface TabComponent {
+  key: TabKey;
+  label: string;
+  component: React.ComponentType<TabProps>;
+}
+
+export interface TabProps {
+  searchTerm: string;
+  onDataCount: (count: number) => void;
+}
