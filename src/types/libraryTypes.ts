fbff7828021729d4dc246bb383668b019a91243c
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-24 23:13:16 +0800

    Added add action & profile handler on types

--- src/types/libraryTypes.ts
+++ src/types/libraryTypes.ts
@@ -17,4 +17,6 @@ export interface TabComponent {
 export interface TabProps {
   searchTerm: string;
   onDataCount: (count: number) => void;
+  setAddAction?: (action: (() => void) | null) => void;
+  onShowingProfile: (showing: boolean) => void;
 }
